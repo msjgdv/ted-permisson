7ecc61228f604fa52420c7a8de22a838397b4fd2
msjgdv <68226715+msjgdv@users.noreply.github.com>
2024-08-30 17:41:59 +0900

    Add files via upload

---
+++ app/src/main/java/com/gun0912/tedpermissiondemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.gun0912.tedpermissiondemo;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+
+
+        new TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
+              //  .setGotoSettingButton(false)
+              //  .setDeniedCloseButtonText(R.string.custom_close_text)
+                .check();
+
+
+    }
+
+
+
+}
---
+++ tedpermission/src/main/java/com/gun0912/tedpermission/TedInstance.java
@@ -0,0 +1,59 @@
+package com.gun0912.tedpermission;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.gun0912.tedpermission.busevent.BusProvider;
+import com.gun0912.tedpermission.busevent.PermissionEvent;
+import com.squareup.otto.Subscribe;
+
+/**
+ * Created by TedPark on 16. 2. 17..
+ */
+public class TedInstance {
+
+    public PermissionListener listener;
+    public String[] permissions;
+    public String denyMessage;
+    public boolean hasSettingBtn = true;
+    public int deniedCloseButtonText=R.string.close;
+    Context context;
+
+
+    public TedInstance(Context context) {
+
+        this.context = context;
+
+        BusProvider.getInstance().register(this);
+    }
+
+
+    public void checkPermissions() {
+
+        //start transparent activity
+
+        Intent intent = new Intent(context, TedPermissionActivity.class);
+        intent.putExtra(TedPermissionActivity.EXTRA_PERMISSIONS, permissions);
+        intent.putExtra(TedPermissionActivity.EXTRA_DENY_MESSAGE, denyMessage);
+        intent.putExtra(TedPermissionActivity.EXTRA_PACKAGE_NAME, context.getPackageName());
+        intent.putExtra(TedPermissionActivity.EXTRA_SETTING_BUTTON, hasSettingBtn);
+        intent.putExtra(TedPermissionActivity.EXTRA_DENIED_DIALOG_CLOSE_TEXT, deniedCloseButtonText);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+
+    @Subscribe
+    public void onPermissionResult(PermissionEvent event) {
+
+        if (event.hasPermission()) {
+            listener.onPermissionGranted();
+        } else {
+            listener.onPermissionDenied(event.getDeniedPermissions());
+        }
+
+
+    }
+
+}
